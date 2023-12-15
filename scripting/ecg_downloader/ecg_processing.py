c92bd267b7d7417609e3c1fcd0d61ffa106bdc74
Joao Avanzini <falejoaovictor@hotmail.com>
2023-12-15 15:16:22 +0000

    Added final blob generator to each subject

--- scripting/ecg_downloader/ecg_processing.py
+++ scripting/ecg_downloader/ecg_processing.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import pandas as pd
 import numpy as np
@@ -30,23 +29,40 @@ class ECGProcessor:
             blob_df = pd.DataFrame({'ECG': blob})
             blob_df.to_csv(f"{output_dir}/subject_{case_id}_blob_{i}.csv", index=False)
 
+        all_blobs_df = pd.concat([pd.read_csv(f"{output_dir}/subject_{case_id}_blob_{i}.csv") for i in range(1, len(blobs) + 1)], ignore_index=True)
+        all_blobs_df.to_csv(f"{output_dir}/all_subject_{case_id}_blobs.csv", index=False)
+
         return blobs
 
     def run_processing(self):
         all_dataframes = []
         total_blobs_generated = 0
-
+    
         for case_id in range(*self.case_id_range):
             blobs = self.process_case(case_id)
             total_blobs_generated += len(blobs)
             print(f"Case ID {case_id}: Total number of generated blobs: {len(blobs)}")
-
+    
+            if len(blobs) < self.blobs_per_patient:
+                print(f"Warning: Insufficient blobs found for case {case_id}. Trying the next case...")
+                adjusted_end_case = self.case_id_range[1] + 1
+                print(f"Adjusting end_case to {adjusted_end_case}")
+                self.case_id_range = (self.case_id_range[0], adjusted_end_case)
+                continue
+    
             for i, blob in enumerate(blobs, 1):
-                blob_df = pd.read_csv(f"./data/output_blobs_case_{case_id}/subject_{case_id}_blob_{i}.csv")
-                all_dataframes.append(blob_df)
-
-        merged_df = pd.concat(all_dataframes, ignore_index=True)
-        merged_df.to_csv("./data/merged_ecg_data.csv", index=False)
-
-        print(f"Total blobs generated across all cases: {total_blobs_generated}")
-        print("Export completed.")
+                blob_file_path = f"./data/output_blobs_case_{case_id}/subject_{case_id}_blob_{i}.csv"
+                if os.path.exists(blob_file_path):
+                    blob_df = pd.read_csv(blob_file_path)
+                    all_dataframes.append(blob_df)
+                else:
+                    print(f"Warning: Blob file not found for case {case_id}, blob {i}")
+    
+        if all_dataframes:
+            merged_df = pd.concat(all_dataframes, ignore_index=True)
+            merged_df.to_csv("./data/merged_ecg_data.csv", index=False)
+    
+            print(f"Total blobs generated across all cases: {total_blobs_generated}")
+            print("Export completed.")
+        else:
+            print("No blobs found. Check the input data.")
